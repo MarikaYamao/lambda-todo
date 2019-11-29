efd6208b8f8a4cc89daf9e3559fc6b94d150d3f5
やまお <>
2019-11-29 09:56:50 +0900

    list修正

--- src/components/List.js
+++ src/components/List.js
@@ -3,13 +3,13 @@ import * as appConfig from '../appConfig'
 import axios from 'axios'
 
 const List = (props) => {
-    const [complete, setComplete] = useState(false)
+    const [complete, setComplete] = React.useState(false)
     const updateTodo = () => {
         
     }
     
     return(
-        <li className={props.class} key={props.key}><button type="button" onClick={}/>{props.content}</li>
+        <li className={props.class} key={props.key}><button type="button"/>{props.content}</li>
     )
 }
 export default List
